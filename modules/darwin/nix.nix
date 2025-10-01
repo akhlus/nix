fdf4831c3d966bbf73e0ebce94b0e98b5381f6ea
akhlus <93236986+akhlus@users.noreply.github.com>
2025-10-01 22:26:51 +0100

    added repo to flake registry

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -9,6 +9,11 @@
       experimental-features = ["nix-command" "flakes"];
       trusted-users = [username];
     };
+    registry."akhlus".to = {
+      owner = "akhlus";
+      repo = "nix";
+      type = "github";
+    };
   };
   nixpkgs.config.allowUnfree = true;
 }
