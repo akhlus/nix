9509c44f458146b64ebb60a9152a1f2e84ca0732
akhlus <93236986+akhlus@users.noreply.github.com>
2025-10-01 17:28:11 +0100

    added templates

--- flake.nix
+++ flake.nix
@@ -13,12 +13,7 @@
       inputs.home-manager.follows = "home-manager";
     };
   };
-  outputs = inputs @ {self, ...}: let
-    mkTemplate = name: description: {
-      inherit description;
-      path = ./templates/${name};
-    };
-  in {
+  outputs = inputs @ {self, ...}: {
     darwinModules = rec {
       darwin.imports = [
         inputs.nix-homebrew.darwinModules.default
@@ -44,11 +39,21 @@
       default.imports = nixos.imports ++ home.imports;
     };
 
-    templates = {
+    templates = let
+      mkTemplate = name: description: {
+        inherit description;
+        path = ./templates/${name};
+      };
+    in {
+      darwin = mkTemplate "darwin" "Darwin system flake";
       go = mkTemplate "go" "Dev tools for golang";
       go-package = mkTemplate "go-package" "Flake to build golang package with necessary dev tools";
+      home = mkTemplate "home" "Home-manager config flake";
+      jovian = mkTemplate "jovian" "Nixos system with jovian setup";
       latex = mkTemplate "latex" "Flake to write LaTeX doc locally";
+      nixos = mkTemplate "nixos" "Simple nixos config flake";
       minimal = mkTemplate "minimal" "Minimal flake";
+      mobile = mkTemplate "mobile" "Mobile nixos config";
       python = mkTemplate "python" "Dev tools for python";
       shell = mkTemplate "shell" "Empty devShell flake";
     };
