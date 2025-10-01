6fd65ae63fbb9cea6987f29ca28d63c56c5b3dc6
akhlus <93236986+akhlus@users.noreply.github.com>
2025-10-01 22:46:01 +0100

    changed dconf default

--- modules/home/de/default.nix
+++ modules/home/de/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: let
   cfg = config.hMods.de;
@@ -10,7 +9,7 @@ in {
     lib.optionals cfg.enableDconf [./dconf.nix]
     ++ lib.optionals cfg.enablePM [./pm.nix];
   options.hMods.de = {
-    enableDconf = lib.mkEnableOption "dconf settings management" // {default = !pkgs.stdenv.isDarwin;};
+    enableDconf = lib.mkEnableOption "dconf settings management" // {default = false;};
     enablePM = lib.mkEnableOption "plasma-manager file" // {default = false;};
   };
 }
