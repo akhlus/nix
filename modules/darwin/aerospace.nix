6336235a271d0868f6cd5b3e97305a7be5caa0d2
akhlus <93236986+akhlus@users.noreply.github.com>
2025-10-04 03:19:43 +0100

    changed aerospace and zed options

--- modules/darwin/aerospace.nix
+++ modules/darwin/aerospace.nix
@@ -7,11 +7,6 @@
 in {
   options.dMods.aerospace = {
     enable = lib.mkEnableOption "aerospace" // {default = true;};
-    enableDefaults = lib.mkEnableOption "default settings" // {default = true;};
-    extraSettings = lib.mkOption {
-      description = "Extra settings to add";
-      default = {};
-    };
   };
   config = {
     services.aerospace = lib.mkIf cfg.enable {
